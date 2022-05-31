b2ef66d5d04bc918f5b578038609bd6f42ae6e3b
FlutLab.io <>
2022-05-31 11:25:22 +0000

    penambahan profil

---
+++ lib/screen/profil_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProfilScreen extends StatefulWidget {
+  const ProfilScreen({Key? key}) : super(key: key);
+  _ProfilScreenState createState() => _ProfilScreenState();
+}
+
+class _ProfilScreenState extends State<ProfilScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
